da573b50fd01ed5cf40ad6546bd06ecaf85756d3
Alejandro Casillas <deal.paradize@hotmail.com>
2017-03-11 21:24:35 -0700

    feat: SpaceHeroes init

---
+++ app/src/main/java/mx/edu/ittepic/tpdm_u2_practica3/Principal.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.tpdm_u2_practica3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
